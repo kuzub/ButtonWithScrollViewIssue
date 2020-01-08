fa10710051baaaa5bfb94456bd73302fc81c4c06
Mykola Kuzub <kuzub@hotmail.com>
2020-01-08 10:33:52 +0200

    CustomScrollViewRenderer

---
+++ ButtonWithScrollViewIssue.Android/CustomScrollViewRenderer.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Android.Content;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+
+[assembly: ExportRenderer(typeof(ButtonWithScrollViewIssue.CustomScrollView), typeof(ButtonWithScrollViewIssue.Droid.CustomScrollViewRenderer))]
+namespace ButtonWithScrollViewIssue.Droid
+{
+    public class CustomScrollViewRenderer : ScrollViewRenderer
+    {
+        public CustomScrollViewRenderer(Context context) : base(context)
+        {
+        }
+    }
+}
